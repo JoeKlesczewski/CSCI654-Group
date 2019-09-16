65042e2c0e9c79225584d04aea20086562b45ab9
Joseph Klesczewski <jak5779@g.rit.edu>
2019-09-16 15:04:13 -0400

    Set inputs.

--- task1.cpp
+++ task1.cpp
@@ -1,8 +1,17 @@
 #include <climits>
-
+#include <iostream>
+#include <string>
 
 
 int main(int argc, char *argv)
 {
+    if(argc < 3)
+    {
+        std::cout << "ERROR: Not enough arguments." << std::endl;
+    }
+
+    long nonce = LONG_MIN;
+    std::string bhashstr(argv[1]);
+    std::string thashstr(argv[2]);
     
 }
