a8f1e4b7bfd9ad357dd6a36a07b276f4a57fe4b9
Joseph Klesczewski <jak5779@g.rit.edu>
2019-09-16 15:45:10 -0400

    Runs now. Debugging.

--- task1.cpp
+++ task1.cpp
@@ -2,16 +2,29 @@
 #include <iostream>
 #include <string>
 
+#include "sha256.h"
 
-int main(int argc, char *argv)
+
+int main(int argc, char **argv)
 {
     if(argc < 3)
     {
         std::cout << "ERROR: Not enough arguments." << std::endl;
+        return 2;
     }
 
     long nonce = LONG_MIN;
-    std::string bhashstr(argv[1]);
-    std::string thashstr(argv[2]);
-    
+    std::string bhashstr = argv[1];
+    std::string thashstr = argv[2];
+    std::string hashstr;
+    std::cout << "BlockHash: " << bhashstr << std::endl;
+    std::cout << "TargetHash: "<< thashstr << std::endl;
+    std::cout << "Performing Proof-of_work...wait..." << std::endl;
+    do {
+        ++nonce;
+        // Question: should to_string really be a decimal string?? It is in the example...
+        hashstr = sha256(sha256(bhashstr + std::to_string(nonce)));
+    } while (hashstr < thashstr && nonce < LONG_MAX);
+    std::cout << "Resulting Hash: " << hashstr << std::endl;
+    std::cout << "Nonce:" << nonce << std::endl;
 }
