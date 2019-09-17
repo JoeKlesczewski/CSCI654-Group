beb421dec1ec6eebf2e00c3f5975f45be4afa12a
Joseph Klesczewski <jak5779@g.rit.edu>
2019-09-17 12:45:36 -0400

    Cleanup of task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -13,24 +13,24 @@ int main(int argc, char **argv)
         return 2;
     }
 
-    int32_t nonce = INT32_MIN;
+    // Read inputs
     std::string bhashstr = argv[1];
     std::string thashstr = argv[2];
-    std::string hashstr;
+    // User output
     std::cout << "BlockHash: " << bhashstr << std::endl;
     std::cout << "TargetHash: "<< thashstr << std::endl;
     std::cout << "Performing Proof-of_work...wait..." << std::endl;
-//    do {
-//        ++nonce;
-//        // Question: should to_string really be a decimal string?? It is in the example...
-//        hashstr = sha256(sha256(bhashstr + std::to_string(nonce)));
-//    } while (hashstr < thashstr);
+    // Set up loop variables
+    int32_t nonce = INT32_MIN;
+    std::string hashstr;
+    // Find a nonce that gives a hash less than the target hash
     for(nonce = INT32_MIN; nonce < INT32_MAX; ++nonce)
     {
         // Question: should to_string really be a decimal string?? It is in the example...
         hashstr = sha256(sha256(bhashstr + std::to_string(nonce)));
         if(hashstr < thashstr) { break; }
     }
+    // Report results
     std::cout << "Resulting Hash: " << hashstr << std::endl;
     std::cout << "Nonce:" << nonce << std::endl;
 }
